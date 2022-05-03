a2d6442bd2c5b5c490298b11a32b6e17c30a5429
ppatna <35831314+ppatna@users.noreply.github.com>
2022-05-03 23:41:17 +0700

    update

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+
+
+import React, { FC, useEffect, useState } from 'react';
+
+const Home: React.FC = () => {
+    return (
+       <div>
+           <p>Vending Machine</p>
+       </div>
+    );
+};
+
+export default Home;
