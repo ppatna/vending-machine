7ccdd7e75280e8f2fab4af1753b1e68a9646abf5
ppatna <35831314+ppatna@users.noreply.github.com>
2022-05-12 20:55:13 +0700

    backup

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,32 +1,96 @@
 
 
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useState } from 'react';
+import './home.css';
+import _ from 'lodash';
+
+import coke from '../../img/cola.png'
+import drink from '../../img/drink.png'
+import water from '../../img/water.png'
 
 
-const product = [
+const products = [
     {
+        "id":1,
         "code_id":"P001",
-        "name":"Coke",
+        "name":"Coke A1",
         "price":15,
-        "amount":10
+        "amount":10,
+        "src": coke,
+        "className":"one"
     },
     {
+        "id":2,
         "code_id":"P002",
-        "name":"Milk",
+        "name":"Coke A2",
         "price":15,
-        "amount":10
+        "amount":10,
+        "src": coke,
+        "className":"one"
     },
     {
+        "id":3,
         "code_id":"P003",
-        "name":"Coconut water",
+        "name":"Coke A3",
         "price":15,
-        "amount":20
+        "amount":20,
+        "src": coke,
+        "className":"one"
+
     },
     {
+        "id":4,
         "code_id":"P004",
-        "name":"Coconut water",
+        "name":"Fruit Mix A1",
         "price":25,
-        "amount":25
+        "amount":25,
+        "src": drink,
+        "className":"two"
+    },
+    {
+        "id":5,
+        "code_id":"P005",
+        "name":"Fruit Mix A2",
+        "price":25,
+        "amount":25,
+        "src": drink,
+        "className":"two"
+    },
+    {
+        "id":6,
+        "code_id":"P006",
+        "name":"Fruit Mix A3",
+        "price":25,
+        "amount":25,
+        "src": drink,
+        "className":"two"
+    },
+    {
+        "id":7,
+        "code_id":"P007",
+        "name":"Water A1",
+        "price":35,
+        "amount":35,
+        "src": water,
+        "className":"three"
+    },
+    {
+        "id":8,
+        "code_id":"P008",
+        "name":"Water A2",
+        "price":35,
+        "amount":25,
+        "src": water,
+        "className":"three"
+    },
+    {
+        "id":9,
+        "code_id":"P009",
+        "name":"Water A3",
+        "price":35,
+        "amount":25,
+        "src": water,
+        "className":"three"
     }
 ]
     
@@ -34,60 +98,123 @@ const product = [
 const Home: React.FC = () => {
 
     const [codeId, setCodeID] = useState('');
-    const [productName, setProductName]  = useState('');
-
-    const [fiveCoin, setFiveCoin] = useState(0);
-    const [tenCoin, setTenCoin] = useState(0);
-    const [twentyCoin, setTwentyCoin] = useState(0);
+    const [productName, setProductName]  = useState(String);
+    const [productPrice, setProductPrice]  = useState(Number);
+    const [productAmount, setProductAmount]  = useState(Number);
     const [totalSum, setTotalSum] = useState(0);
+    const [productSelect, setProductSelect] = useState<any[]>([]);
+    const [creditMoney, setCreditMoney] = useState(200);
+    const [balanceMoney, setฺBalanceMoney] = useState(0);
+    const [isShowAlert, setIsShowAlert] = useState(false);
 
+    const handleChange = (e:any,id:number) => {   
+    
+        let product = products.filter((item:any) => item.id === id)[0];
 
-    const onSubmit = (e:any) =>{
-        e.preventDefault();
+        setProductName(product.name)
+        setProductPrice(product.price)
 
-        // let totalOfMoney = fiveCoin + tenCoin;
-
-        console.log("fiveCoin: "+fiveCoin);
-
-        // console.log("10"+tenCoin);
-        // console.log("20"+twentyCoin);
-        // setTotalSum(totalOfMoney)
+       let itemsSelect =  _.filter(productSelect, { 'id': id });
         
+           if(itemsSelect.length < 1){
+                productSelect.push({
+                        "id":id,
+                        "count":1
+                })
+
+           }else{
+
+                _.forEach(productSelect,(values,idx)=>{
+                    if(values.id === id){
+                       values.count +=  1    
+                    }
+                });
+           }
+
+           setProductSelect(productSelect)
+           countNum()
+
+    };
+
+    const countNum = () => {
+        let total = 0
+        let countItems = 0;
+        let countBalance = 0;
+
+        _.forEach(productSelect,(values,idx)=>{
+            let product = products.filter((item:any) => item.id === values.id)[0];
+            if(product){
+
+                countItems += values.count
+                total += product.price * values.count ;   
+
+                if(total > creditMoney ){
+                    setIsShowAlert(true)
+                }else{
+                    countBalance  =  creditMoney - total;
+                }
+            }
+        });
+
+        setProductAmount(countItems)
+        setTotalSum(total)
+        setฺBalanceMoney(countBalance)
     }
 
-    const handleChange = (e: any, setter: (data: any) => void) => {   
-        console.log("e.target.value:"+e.target.value);
-        
-        setter(e.target.value);
-    };
+    const refreshPage = ()=>{
+        window.location.reload();
+     }
 
     return (
        <div>
-           <p>Vending Machine</p>
-           <form onSubmit={onSubmit}>
-            <label>
-                <p> Product Code:
-                <input type="text" name="codeId" id="CodeId"  onChange={e => handleChange(e, setCodeID)}/>
-                </p>
-            </label>
-            <label>
-                Top Up:
-                <button value="5" name="fiveCoin" id="fiveCoin" onClick={e => handleChange(e, setFiveCoin)} >5</button>
-                <button  value="10" name="tenCoin" id="tenCoin" onClick={e => handleChange(e, setTenCoin)} >10</button>
-                <button  value="20" name="twentyCoin" id="twentyCoin" onClick={e => handleChange(e, setTwentyCoin)} >20</button>
-            </label>
-            <p>
-            <label>
-                Credit (Money):  Baht
-            </label>
-            </p>
-           
-            <p>
-            <input type="submit" value="Submit" />
-            </p>
-           
-            </form>
+            <div className='head-title'>
+                    <h1 >Vending Machine</h1>
+                    <span>select what you eat</span>
+            </div>
+            <div className="machine">
+            <div className="items">
+                 {products && products.map((item: any, key: number) => (
+                    <>
+                        <div className="item">
+                            <img className="image eA1" src={item.src} alt="" onClick={e => handleChange(e,item.id)}/>
+                            <p style={{fontSize:'14px'}}> {item.name}</p>
+                            <p> {item.price} ฿</p>
+                        </div>
+                     </>
+          ))}
         </div>
+
+            <div className="order">
+                <div className="container">
+                    <p>Details</p>
+                    <br/>
+                    <p> {productName ? productName : 'Select What You Eat'}</p>
+                    <br/>
+                    <p> Price : {productPrice && productPrice}</p>
+                    <br/>
+                    <p> Amount : {productAmount && productAmount}</p>
+                    <br/>
+                    <p> Total Sum : {totalSum} ฿</p>
+                    <br/>
+                    <p> Credit : {creditMoney} ฿</p>
+                    <br/>
+                    <p> Balance : {balanceMoney} ฿</p>
+                    <br/>
+                    <p style={{color:'red'}}> {isShowAlert && 'please top up'}</p>
+                    <button onClick={refreshPage}> Refresh </button>
+                </div>
+                <div className="coins">
+                    <div className="money">
+                        <div className="circle"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="grab">
+                <div className="rectangle"></div>
+            </div>
+            </div>
+            </div>
     );
 };
 
